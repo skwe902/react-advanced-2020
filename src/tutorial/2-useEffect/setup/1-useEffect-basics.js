30276273737ffb818572dc01d7bc98c2bcab2145
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2022-02-26 21:34:08 +1300

    USeEffect conditional statements

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -4,8 +4,11 @@ import React, { useState, useEffect } from 'react';
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
   useEffect(()=>{
-    document.title = `New Messages(${value})`;
+    if(value >= 1){
+      document.title = `New Messages(${value})`;
+    }
   });
 
   return (
