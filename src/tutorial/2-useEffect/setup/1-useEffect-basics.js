b1532be7471538de1d7ceb40e20f97adec4948a8
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2022-02-26 21:40:01 +1300

    dependency list

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -5,11 +5,13 @@ import React, { useState, useEffect } from 'react';
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     if(value >= 1){
       document.title = `New Messages(${value})`;
     }
-  });
+  }, [value]); //only want to run the useEffect in the initial render add an empty array
+  //now it will run when the value changes (which is in the dependency list)
+  
 
   return (
     <>
